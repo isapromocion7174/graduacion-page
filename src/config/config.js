98e3fdae2d9fa2c1d55e700937940f5fbb1f5528
Joan Corona <joaneliasc86@gmail.com>
2023-12-24 01:13:03 -0400

    base front-end

---
+++ src/config/config.js
@@ -0,0 +1,101 @@
+export const navbarOptions = [
+    {
+        name: "Inicio",
+        path: "/",
+        dropDown: true,
+        dropDownOptions: [
+            {
+                name: "Nuestro ISA",
+                path: "/nuestro-isa",
+            },
+            {
+                name: "Carta de Aceptación",
+                path: "/carta-aceptacion",
+            },
+            {
+                name: "Distribucion por habitacion",
+                path: "/distribucion-habitacion",
+            },
+            {
+                name: "Origen y titulos acuales",
+                path: "/origen-titulos",
+            },
+            {
+                name: "Un dia tipico en ISA",
+                path: "/dia-tipico",
+            },
+        ],
+    },
+    {
+        /*
+    
+        Aniversarios
+        20th Aniversario
+        25th Aniversario
+        35th Aniversario
+        42th Aniversario
+        43th Aniversario
+        50th Aniversario
+    */
+        name: "Aniversarios",
+        path: "/aniversarios",
+        dropDown: true,
+        dropDownOptions: [
+            {
+                name: "20th Aniversario",
+                path: "/20th-aniversario",
+            },
+            {
+                name: "25th Aniversario",
+                path: "/25th-aniversario",
+            },
+            {
+                name: "35th Aniversario",
+                path: "/35th-aniversario",
+            },
+            {
+                name: "42th Aniversario",
+                path: "/42th-aniversario",
+            },
+            {
+                name: "43th Aniversario",
+                path: "/43th-aniversario",
+            },
+            {
+                name: "50th Aniversario",
+                path: "/50th-aniversario",
+            },
+        ],
+    },
+    {
+        /*
+        Miscelaneos
+
+        Encuentros
+        Anecdotas y Jocosidades
+        Programas de Graduacion
+        Otros
+    */
+        name: "Miscelaneos",
+        path: "/miscelaneos",
+        dropDown: true,
+        dropDownOptions: [
+            {
+                name: "Encuentros",
+                path: "/encuentros",
+            },
+            {
+                name: "Anecdotas y Jocosidades",
+                path: "/anecdotas-jocosidades",
+            },
+            {
+                name: "Programas de Graduacion",
+                path: "/programas-graduacion",
+            },
+            {
+                name: "Otros",
+                path: "/otros",
+            },
+        ],
+    },
+];
