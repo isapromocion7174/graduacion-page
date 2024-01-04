7bd31d59eca80b41325bd1bb62bb2f6a7de6d9e9
Joan Corona <joaneliasc86@gmail.com>
2024-01-04 01:32:15 -0400

    change icon error

--- src/components/event/MantenanceLayout.jsx
+++ src/components/event/MantenanceLayout.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import Table from "@/components/table/index.jsx";
-import { FaPlus } from "react-icons/fa6";
 import EventosInput from "./index";
 import { deleteEvento } from "@/api/eventos";
 const MantenanceLayout = ({ title, headers, data, messages }) => {
@@ -57,7 +56,20 @@ const MantenanceLayout = ({ title, headers, data, messages }) => {
                 }}
             >
                 {" "}
-                <FaPlus />
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="0px"
+                    y="0px"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        d="M 11 2 L 11 11 L 2 11 L 2 13 L 11 13 L 11 22 L 13 22 L 13 13 L 22 13 L 22 11 L 13 11 L 13 2 Z"
+                    ></path>
+                </svg>
             </button>
         </>
     );
